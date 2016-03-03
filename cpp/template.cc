4ea15664ed5e7710f2b2e1995ad5635b5454282d
dat <dat.hoang@gmail.com>
2016-03-02 17:52:59 -0800

    Add basic template file for C++ programs.

---
+++ cpp/template.cc
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+// basic CPP template file
+
+int main(int argc, char* argv[]) { 
+    
+    return 0;
+}
